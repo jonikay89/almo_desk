3d47076366d2aa46d9a5a7d88593ce3f7471bafb
yoni89 <yonik@gini-apps.com>
2026-04-11 20:42:04 +0300

    add kvo

--- src/core/NSObject.js
+++ src/core/NSObject.js
@@ -2,6 +2,8 @@ class NSObject {
     constructor() {
         this._hash = NSObject._nextHash++;
         this._description = null;
+        this._observers = new Map();
+        this._keyPaths = new Map();
     }
 
     get hash() {
@@ -60,6 +62,98 @@ class NSObject {
     autorelease() {
         return this;
     }
+
+    addObserver(observer, keyPath, options = {}) {
+        if (!this._observers.has(keyPath)) {
+            this._observers.set(keyPath, []);
+        }
+        const observers = this._observers.get(keyPath);
+        if (!observers.find(o => o.observer === observer)) {
+            observers.push({
+                observer,
+                options: {
+                    newValue: true,
+                    oldValue: false,
+                    ...options
+                }
+            });
+        }
+        if (!this._keyPaths.has(keyPath)) {
+            this._keyPaths.set(keyPath, this._getValueForKeyPath(keyPath));
+        }
+    }
+
+    removeObserver(observer, keyPath) {
+        if (this._observers.has(keyPath)) {
+            this._observers.set(keyPath, 
+                this._observers.get(keyPath).filter(o => o.observer !== observer)
+            );
+        }
+    }
+
+    removeAllObservers() {
+        this._observers.clear();
+    }
+
+    willChangeValue(keyPath) {
+        if (this._observers.has(keyPath)) {
+            this._keyPaths.set(keyPath, this._getValueForKeyPath(keyPath));
+        }
+    }
+
+    didChangeValue(keyPath) {
+        if (this._observers.has(keyPath)) {
+            const newValue = this._getValueForKeyPath(keyPath);
+            const oldValue = this._keyPaths.get(keyPath);
+            const observers = this._observers.get(keyPath);
+            
+            for (const { observer, options } of observers) {
+                const change = {
+                    object: this,
+                    keyPath,
+                    newValue: options.newValue ? newValue : undefined,
+                    oldValue: options.oldValue ? oldValue : undefined
+                };
+                
+                if (observer.observeValueForKeyPath) {
+                    observer.observeValueForKeyPath(change);
+                } else if (typeof observer === 'function') {
+                    observer(change);
+                }
+            }
+        }
+    }
+
+    _getValueForKeyPath(keyPath) {
+        const keys = keyPath.split('.');
+        let value = this;
+        for (const key of keys) {
+            if (value === null || value === undefined) return undefined;
+            value = value[key];
+        }
+        return value;
+    }
+
+    _setValueForKeyPath(keyPath, value) {
+        const keys = keyPath.split('.');
+        const lastKey = keys.pop();
+        let target = this;
+        
+        for (const key of keys) {
+            if (target === null || target === undefined) return;
+            target = target[key];
+        }
+        
+        if (target !== null && target !== undefined) {
+            target[lastKey] = value;
+        }
+    }
+
+    static keyPathsForValuesAffecting(keyPath) {
+        return new Set();
+    }
+
+    static setKeysTriggeringNotificationsForKeyValueObserving(keys) {}
 }
 
 export default NSObject;
